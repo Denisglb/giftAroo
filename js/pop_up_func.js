d46a39db7286bf93ab635d924668ca0a0a409849
Denis <denisglb@yahoo.co.uk>
2018-02-11 11:45:29 +0000

    image updates

--- js/pop_up_func.js
+++ js/pop_up_func.js
@@ -1,7 +1,7 @@
  $(window).load(function() {
                 new $.popup({                
                     title       : '',
-                    content         : '<a href="all_tours_list.html"><img src="img/popup_img.jpg" alt="" class="pop_img"><h3 id="pop_msg">This week special offer <strong>Free Teddy Bear</strong> on all Orders from LBS!</h3></a><small>Autoclose delay in 5 seconds.</small>', 
+                    content         : '<a href="all_tours_list.html"><img src="img/popup_img.jpg" alt="" class="pop_img"><h3 id="pop_msg">LBS special offer <strong>Free delivery</strong> for Valentines Day</h3></a><small>Autoclose delay in 5 seconds.</small>', 
 					html: true,
 					autoclose   : true,
 					closeOverlay:true,
